7b55ae25dd8a2749aac582f03a8df8d343a4b344
Chedy Chaaben <40511388+chedychaaben@users.noreply.github.com>
2025-04-04 00:35:58 +0100

    Update user pour reparer mongose vers mysql

--- models/User.js
+++ models/User.js
@@ -2,33 +2,65 @@ import { DataTypes } from "sequelize";
 import sequelize from "../db.js";
 
 const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    trim: true,
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
     validate: {
-      isEmail: true
-    }
+      isEmail: true,
+    },
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      len: [8, 255]
-    }
+      len: [8, 255],
+    },
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  profilePicture: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    validate: {
+      isInt: true,
+      min: 1,
+    },
+  },
+  profile: {
+    type: DataTypes.TEXT,
+    allowNull: true,
   },
   isAdmin: {
     type: DataTypes.BOOLEAN,
     defaultValue: false,
   },
-  // other fields like phoneNumber, profilePicture, etc.
 }, {
   timestamps: true,
 });
+
+// Associations (if you have Ride model)
+User.associate = (models) => {
+  User.hasMany(models.Ride, { foreignKey: "createdBy", as: "ridesCreated" });
+  User.belongsToMany(models.Ride, { through: "UserRides", as: "ridesJoined" });
+};
 
 export default User;
