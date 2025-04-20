5b7b833a7fa2b627b3d69e8164f937c598d36765
Chedy Chaaben <chedychaaben@gmail.com>
2025-04-20 08:56:44 +0100

    update

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { banUser, warnUser } from "../controllers/admin.js";
+import { banUser, warnUser, unbanUser } from "../controllers/admin.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.patch("/warn/:id", verifyAdmin, warnUser);
-router.delete("/ban/:id", verifyAdmin, banUser); 
+router.delete("/ban/:id", verifyAdmin, banUser);
+router.delete("/unban/:id", verifyAdmin, unbanUser);
 
 export default router;
